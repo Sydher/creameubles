3e404d2d49eca76a33837c2c4d96e5713f65ee2b
Sydher <hugoc.pro@gmail.com>
2019-08-02 15:28:59 +0200

    Page d'accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
 // Liste des routes de l'application
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: AccueilComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
