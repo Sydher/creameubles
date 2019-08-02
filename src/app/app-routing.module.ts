a7f17f88f03cff7027eaf7eebb1b896c8945da9e
Sydher <hugoc.pro@gmail.com>
2019-08-02 17:19:47 +0200

    Contenu v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
+import { MagasinComponent } from './pages/magasin/magasin.component';
+import { ProduitsComponent } from './pages/produits/produits.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 // Liste des routes de l'application
 const routes: Routes = [
-    { path: '', component: AccueilComponent }
+    { path: '', component: AccueilComponent },
+    { path: 'notre-magasin', component: MagasinComponent },
+    { path: 'nos-produits', component: ProduitsComponent },
+    { path: 'nous-contacter', component: ContactComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
+import { MagasinComponent } from './pages/magasin/magasin.component';
+import { ProduitsComponent } from './pages/produits/produits.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        AccueilComponent
+        AccueilComponent,
+        MagasinComponent,
+        ProduitsComponent,
+        ContactComponent
     ],
     imports: [
         BrowserModule,
